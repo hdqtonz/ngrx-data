4e97ba5eeeef2da5c8c6b595427a785fd869690d
hdqtonz <hiten.qtonz@yahoo.com>
2026-02-27 15:24:58 +0530

    ngrx data - 1 file changed - misc change sorting coparer

--- src/app/entity-metadata.ts
+++ src/app/entity-metadata.ts
@@ -2,6 +2,7 @@ import { EntityMetadataMap, EntityDataModuleConfig } from '@ngrx/data';
 
 const entityMetadata: EntityMetadataMap = {
   Post: {
+    // sortComparer: (a, b) => b.id - a.id,
     // entityDispatcherOptions: {
     //   optimisticUpdate: true,
     //   optimisticDelete: true,
