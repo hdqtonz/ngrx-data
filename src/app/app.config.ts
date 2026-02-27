6d614f4ef05407a67d221bb8fdbd087543cceb97
hdqtonz <hiten.qtonz@yahoo.com>
2026-02-27 12:11:44 +0530

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideEntityData, withEffects } from '@ngrx/data';
+import { entityConfig } from './entity-metadata';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    // ngrx-store
+    provideStore(),
+    provideEffects(),
+    // ngrx-data
+    provideEntityData(entityConfig, withEffects()),
+    // DevTools
+    provideStoreDevtools(),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./home/home').then((c) => c.Home) },
+  {
+    path: 'posts',
+    loadComponent: () => import('./posts/posts-list/posts-list').then((c) => c.PostsList),
+  },
+  {
+    path: 'posts/add',
+    loadComponent: () => import('./posts/add-post/add-post').then((c) => c.AddPost),
+  },
+  {
+    path: 'posts/edit/:id',
+    loadComponent: () => import('./posts/edit-post/edit-post').then((c) => c.EditPost),
+  },
+  {
+    path: 'posts/details/:id',
+    loadComponent: () => import('./posts/single-post/single-post').then((c) => c.SinglePost),
+  },
+];
